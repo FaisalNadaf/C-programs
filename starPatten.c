cda6f29b6d7b2717494dbf6a49858e951d78a693
faisal <faisalfnadaf@gmail.com>
2024-02-17 21:43:04 +0530

    code

--- starPatten.c
+++ starPatten.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 void main()
 {
-    int i, j;
-    for (i = 0; i < 5; i++)
+    int i, j, n;
+    printf("Enter The Rows Count");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
     {
         for (j = 0; j < i; j++)
         {
-            printf('j');
+            printf("*");
         }
+        printf("\n");
     }
 }
